775cc313e55e649a9141c2fd836c100e6d8a9072
nishant.tomer <nishantfun4@gmail.com>
2016-04-17 22:00:57 +0530

    update .gitignore

--- app.js
+++ app.js
@@ -4,13 +4,23 @@ var express = require("express")
     ,app = express()
     ,env = require("./config/" + app.get("env"))
     ,assets = path.join(env.cwd,"/assets")
-    ,views = path.join(env.cwd,"/views");
+    ,views = path.join(env.cwd,"/views")
+    ,passport = require('passport')
+    ,session = require('express-session');
 
-var mongoose = require("mongoose");
-    mongoose.connect(env.db_url);
+require('dotenv').load();
+require('./controllers/passport')(passport);
 
 app.set("views", views);
 app.set("view engine", "jade");
+app.use(session({
+	secret: 'fccvotingappsecret',
+	resave: false,
+	saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(assets));
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
@@ -18,12 +28,14 @@ app.use(express.methodOverride());
 app.use(express.cookieParser('freecodecamp'));
 app.use(express.session());
 
+var mongoose = require("mongoose");
+    mongoose.connect(process.env.DB_URL);
 
 
 fs.readdirSync('./controllers').forEach(function (file) {
   if(file.substr(-3) == '.js') {
       route = require('./controllers/' + file);
-      route.controller(app);
+      route.controller(app,passport);
   }
 });
 
